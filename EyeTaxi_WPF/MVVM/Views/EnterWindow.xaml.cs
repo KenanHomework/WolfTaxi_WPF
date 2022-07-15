24f00f50ccf32d4a0b63d6fbbbc6b2bfb0a429ce
kenan <86625368+kenanysbv@users.noreply.github.com>
2022-07-15 20:52:23 +0400

    Applt mvvm

---
+++ EyeTaxi_WPF/MVVM/Views/EnterWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EyeTaxi_WPF.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for EnterWindow.xaml
+    /// </summary>
+    public partial class EnterWindow : Window
+    {
+        public EnterWindow()
+        {
+            InitializeComponent();
+            Frame.Navigate(new LoginPage());
+        }
+        private void ResizeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            if (sender is Button btn)
+            {
+                switch (btn.Content.ToString())
+                {
+                    case "_":
+                        Application.Current.MainWindow.WindowState = WindowState.Minimized;
+                        break;
+                    case "❒":
+                        if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+                            Application.Current.MainWindow.WindowState = WindowState.Normal;
+                        else
+                            Application.Current.MainWindow.WindowState = WindowState.Maximized;
+                        break;
+                    case "X":
+                        Application.Current.Shutdown();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
---
+++ EyeTaxi_WPF/MVVM/Views/LoginPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using EyeTaxi_WPF.Interfaces;
+using EyeTaxi_WPF.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EyeTaxi_WPF.MVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page, IEnterPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+            => RegxService.CheckControl(ref Username, 3, Color.FromRgb(237, 236, 239), "^([A-Za-z0-9]){4,20}$");
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+            => RegxService.CheckControl(ref Password, 8, Color.FromRgb(237, 236, 239), "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
+
+        public bool AllInfoCorrect() 
+            => RegxService.CheckControl(ref Password, 8, Color.FromRgb(237, 236, 239), "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$") &&
+                             RegxService.CheckControl(ref Username, 3, Color.FromRgb(237, 236, 239), "^([A-Za-z0-9]){4,20}$");
+    }
+}
