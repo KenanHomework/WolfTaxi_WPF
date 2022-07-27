e56dfd158dd89892ce67efc6d488d3fe2fdef658
kenan <86625368+kenanysbv@users.noreply.github.com>
2022-07-27 16:37:29 +0400

    Test

--- EyeTaxi_WPF/MVVM/ViewModels/EnterSecurityVM.cs
+++ EyeTaxi_WPF/MVVM/ViewModels/EnterSecurityVM.cs
@@ -8,7 +8,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WolfTaxi_WPF.MVVM.ViewModels
+namespace WolfTaxi_WPF.MVVM.ViewModels 
 {
     public class EnterSecurityVM
     {
